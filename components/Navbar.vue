3a91fa360ee9b122a9df6ac3d72ac856c366fbd3
afifrohul <afifmemyself22@gmail.com>
2023-03-28 23:04:48 +0700

    initial

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex mb-4 text-sm lg:text-base">
+    <div class="mr-4 bg-gray-300 p-1 rounded-sm hover:bg-gray-500 transition-all duration-300">
+      <NuxtLink to="/">Home</NuxtLink>
+    </div>
+    <div class=" bg-gray-300 p-1 rounded-sm hover:bg-gray-500 transition-all duration-300">
+      <NuxtLink to="/project">Project</NuxtLink>
+    </div>
+  </div>
+</template>
---
+++ pages/project.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="mt-6">
+    <p class="text-lg lg:text-xl mt-4 font-extrabold">Project</p>
+    <p class="text-sm lg:text-base">Here are the projects I've already completed individually or as a team.</p>
+    <hr class="mt-8">
+    <div class="grid grid-cols-2 gap-4 mt-6">
+      <a href="https://hmifunej.id/" target="_blank">
+        <div class="border-2 rounded-md hover:scale-105 transition-all duration-500 shadow-sm hover:shadow-md ease-linear">
+          <div class="max-w-sm rounded overflow-hidden">
+            <img class="w-full" src="/images/hmifunej.png" alt="hmifunej">
+            <div class="px-2 py-4">
+              <div class="font-bold text-sm lg:text-base mb-2">HMIFUNEJ (TEAM PROJECT)</div>
+              <p class="text-gray-700 text-xs lg:text-sm">
+                Web company profile of hmifunej and more stuff about hmifunej.
+              </p>
+            </div>
+            <div class="px-2 pb-2">
+              <div class="flex align-middle flex-wrap gap-2">
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg class="w-4 mr-1"  viewBox="0 0 106 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M52.897 0C38.791 0 29.975 7.053 26.449 21.16C31.739 14.106 37.911 11.461 44.964 13.225C48.988 14.23 51.864 17.151 55.048 20.382C60.234 25.646 66.237 31.739 79.348 31.739C93.454 31.739 102.27 24.686 105.797 10.579C100.507 17.633 94.335 20.278 87.282 18.514C83.258 17.509 80.382 14.588 77.198 11.357C72.011 6.093 66.009 0 52.897 0ZM26.449 31.739C12.343 31.739 3.527 38.792 0 52.899C5.29 45.845 11.461 43.2 18.514 44.964C22.539 45.971 25.414 48.89 28.598 52.121C33.784 57.385 39.788 63.478 52.898 63.478C67.005 63.478 75.821 56.425 79.348 42.318C74.058 49.372 67.886 52.017 60.833 50.253C56.809 49.248 53.933 46.327 50.749 43.097C45.563 37.832 39.56 31.739 26.449 31.739Z" fill="#1E3050"/>
+                  </svg>
+                  <p class="text-xs">TAILWIND CSS</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-3 mr-1" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"/></svg>
+                  <p class="text-xs">VUEJS</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg class="w-4 mr-1" viewBox="0 0 400 297" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M227.916 81.0871L214.228 104.791L167.413 23.7044L23.6991 272.593H121.003C121.003 285.684 131.615 296.297 144.707 296.297H23.6992C15.2315 296.297 7.40771 291.777 3.17456 284.443C-1.05859 277.109 -1.05816 268.075 3.1757 260.741L146.889 11.8525C151.123 4.51819 158.949 0 167.418 0C175.886 0 183.712 4.51819 187.946 11.8525L227.916 81.0871Z" fill="#1E3050"/>
+                  <path d="M331.66 260.741L241.605 104.791L227.916 81.0869L214.228 104.791L124.183 260.741C119.949 268.074 119.949 277.109 124.182 284.443C128.415 291.777 136.239 296.297 144.707 296.297H311.107C319.576 296.297 327.403 291.782 331.639 284.447C335.874 277.113 335.875 268.076 331.64 260.741H331.66ZM144.707 272.593L227.916 128.494L311.107 272.593L144.707 272.593Z" fill="#1E3050"/>
+                  <path d="M396.045 284.445C391.811 291.778 383.988 296.297 375.521 296.297H311.107C324.198 296.297 334.81 285.684 334.81 272.593H375.472L260.094 72.761L241.605 104.791L227.916 81.0869L239.571 60.9092C243.805 53.5748 251.63 49.0566 260.099 49.0566C268.568 49.0566 276.393 53.5748 280.628 60.9092L396.045 260.741C400.279 268.075 400.279 277.111 396.045 284.445Z" fill="#1E3050"/>
+                  </svg>
+                  <p class="text-xs">NUXTJS</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="https://ifmerch.hmifunej.id/" target="_blank">
+        <div class="border-2 rounded-md hover:scale-105 transition-all duration-500 shadow-sm hover:shadow-md ease-linear">
+          <div class="max-w-sm rounded overflow-hidden">
+            <img class="w-full" src="/images/ifmerch.png" alt="ifmerch">
+            <div class="px-2 py-4">
+              <div class="font-bold text-sm lg:text-base mb-2">IFMERCH (TEAM PROJECT)</div>
+              <p class="text-gray-700 text-xs lg:text-sm">
+                E-commerce of brand hmifunej named IFMERCH.
+              </p>
+            </div>
+            <div class="px-2 pb-2">
+              <div class="flex align-middle flex-wrap gap-2">
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg class="w-4 mr-1"  viewBox="0 0 106 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M52.897 0C38.791 0 29.975 7.053 26.449 21.16C31.739 14.106 37.911 11.461 44.964 13.225C48.988 14.23 51.864 17.151 55.048 20.382C60.234 25.646 66.237 31.739 79.348 31.739C93.454 31.739 102.27 24.686 105.797 10.579C100.507 17.633 94.335 20.278 87.282 18.514C83.258 17.509 80.382 14.588 77.198 11.357C72.011 6.093 66.009 0 52.897 0ZM26.449 31.739C12.343 31.739 3.527 38.792 0 52.899C5.29 45.845 11.461 43.2 18.514 44.964C22.539 45.971 25.414 48.89 28.598 52.121C33.784 57.385 39.788 63.478 52.898 63.478C67.005 63.478 75.821 56.425 79.348 42.318C74.058 49.372 67.886 52.017 60.833 50.253C56.809 49.248 53.933 46.327 50.749 43.097C45.563 37.832 39.56 31.739 26.449 31.739Z" fill="#1E3050"/>
+                  </svg>
+                  <p class="text-xs">TAILWIND CSS</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-3 mr-1" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"/></svg>
+                  <p class="text-xs">VUEJS</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg class="w-4 mr-1" viewBox="0 0 400 297" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M227.916 81.0871L214.228 104.791L167.413 23.7044L23.6991 272.593H121.003C121.003 285.684 131.615 296.297 144.707 296.297H23.6992C15.2315 296.297 7.40771 291.777 3.17456 284.443C-1.05859 277.109 -1.05816 268.075 3.1757 260.741L146.889 11.8525C151.123 4.51819 158.949 0 167.418 0C175.886 0 183.712 4.51819 187.946 11.8525L227.916 81.0871Z" fill="#1E3050"/>
+                  <path d="M331.66 260.741L241.605 104.791L227.916 81.0869L214.228 104.791L124.183 260.741C119.949 268.074 119.949 277.109 124.182 284.443C128.415 291.777 136.239 296.297 144.707 296.297H311.107C319.576 296.297 327.403 291.782 331.639 284.447C335.874 277.113 335.875 268.076 331.64 260.741H331.66ZM144.707 272.593L227.916 128.494L311.107 272.593L144.707 272.593Z" fill="#1E3050"/>
+                  <path d="M396.045 284.445C391.811 291.778 383.988 296.297 375.521 296.297H311.107C324.198 296.297 334.81 285.684 334.81 272.593H375.472L260.094 72.761L241.605 104.791L227.916 81.0869L239.571 60.9092C243.805 53.5748 251.63 49.0566 260.099 49.0566C268.568 49.0566 276.393 53.5748 280.628 60.9092L396.045 260.741C400.279 268.075 400.279 277.111 396.045 284.445Z" fill="#1E3050"/>
+                  </svg>
+                  <p class="text-xs">NUXTJS</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a href="https://magmasmasa.github.io/" target="_blank">
+        <div class="border-2 rounded-md hover:scale-105 transition-all duration-500 shadow-sm hover:shadow-md ease-linear">
+          <div class="max-w-sm rounded overflow-hidden">
+            <img class="w-full" src="/images/magmasmasa.png" alt="magmasmasa">
+            <div class="px-2 py-4">
+              <div class="font-bold text-sm lg:text-base mb-2">MAGMA SMASA</div>
+              <p class="text-gray-700 text-xs lg:text-sm">
+                Web profile of magma class High School 1 Jember.
+              </p>
+            </div>
+            <div class="px-2 pb-2">
+              <div class="flex align-middle flex-wrap gap-2">
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-3 mr-1" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"/></svg>
+                  <p class="text-xs">HTML</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-3 mr-1" viewBox="0 0 384 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193 208.6l-.3.1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6 13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2.1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9 112h242.2z"/></svg>
+                  <p class="text-xs">CSS</p>
+                </div>
+                <div class="flex border-2 p-1 rounded-sm">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 mr-1" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M333.5,201.4c0-22.1-15.6-34.3-43-34.3h-50.4v71.2h42.5C315.4,238.2,333.5,225,333.5,201.4z M517,188.6 c-9.5-30.9-10.9-68.8-9.8-98.1c1.1-30.5-22.7-58.5-54.7-58.5H123.7c-32.1,0-55.8,28.1-54.7,58.5c1,29.3-0.3,67.2-9.8,98.1 c-9.6,31-25.7,50.6-52.2,53.1v28.5c26.4,2.5,42.6,22.1,52.2,53.1c9.5,30.9,10.9,68.8,9.8,98.1c-1.1,30.5,22.7,58.5,54.7,58.5h328.7 c32.1,0,55.8-28.1,54.7-58.5c-1-29.3,0.3-67.2,9.8-98.1c9.6-31,25.7-50.6,52.1-53.1v-28.5C542.7,239.2,526.5,219.6,517,188.6z M300.2,375.1h-97.9V136.8h97.4c43.3,0,71.7,23.4,71.7,59.4c0,25.3-19.1,47.9-43.5,51.8v1.3c33.2,3.6,55.5,26.6,55.5,58.3 C383.4,349.7,352.1,375.1,300.2,375.1z M290.2,266.4h-50.1v78.4h52.3c34.2,0,52.3-13.7,52.3-39.5 C344.7,279.6,326.1,266.4,290.2,266.4z"/></svg>
+                  <p class="text-xs">BOOTSTRAP</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+    <hr class="mt-8">
+  </div>
+</template>
